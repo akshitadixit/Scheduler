b307011d1d3239690c1c6f809690e57443b0acb7
Isha307 <56998032+Isha307@users.noreply.github.com>
2022-10-16 16:12:25 +0530

    Project Scheduler

---
+++ scheduler/models/models.py
@@ -0,0 +1,89 @@
+"""contains user data"""
+from flask_login import UserMixin
+from scheduler import db
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    last_task_id = db.Column(db.Integer)
+    university = db.Column(db.String, nullable=False)
+    courses = db.relationship('Courses', backref='poster')
+
+    def __init__(self, username=None, email=None, password=None, university=None):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.university = university
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
+class Courses(db.Model):
+    __tablename__ = 'courses'
+
+    c_id = db.Column(db.Integer, primary_key=True)
+    course_code = db.Column(db.String, nullable=False)
+    course_name = db.Column(db.String, nullable=False)
+    lecturer = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, course_code=None, course_name=None, lecturer=None, location=None, user_id=None):
+        self.course_code = course_code
+        self.course_name = course_name
+        self.lecturer = lecturer
+        self.location = location
+        self.user_id = user_id
+
+
+class Assignment(db.Model): # Dominic 18/11- created model for assignment db
+    __tablename__ = 'assignments'
+
+    a_id = db.Column(db.Integer, primary_key=True)
+    course_code = db.Column(db.String, db.ForeignKey('courses.course_code'))
+    asg_name = db.Column(db.String, nullable=False)
+    weighting = db.Column(db.Integer)
+    due_date = db.Column(db.Date, nullable=False)
+    due_time = db.Column(db.Time)
+    complete = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, course_code=None, asg_name=None, weighting = 0, due_date=None, due_time=None, complete=False, user_id=None):
+        self.course_code = course_code
+        self.asg_name = asg_name
+        self.weighting = weighting
+        self.due_date = due_date
+        self.due_time = due_time
+        self.complete = complete
+        self.user_id = user_id
+
+
+class Exam(db.Model): # Dominic 18/11- created model for assignment db
+    __tablename__ = 'exams'
+
+    e_id = db.Column(db.Integer, primary_key=True)
+    exam_name=db.Column(db.String, nullable=False)
+    course_code = db.Column(db.String, db.ForeignKey('courses.course_code'))
+    weighting = db.Column(db.Integer)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time)
+    duration = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, course_code=None,exam_name=None, weighting=0, date=None, time=None, duration=None, location=None, user_id=None):
+        self.course_code = course_code
+        self.exam_name=exam_name
+        self.weighting = weighting
+        self.date = date
+        self.time = time
+        self.duration = duration
+        self.location = location
+        self.user_id = user_id
+          
---
+++ scheduler/routes.py
@@ -0,0 +1,220 @@
+"""application routes"""
+from flask import render_template, url_for, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, current_user, login_required
+from scheduler import app, db, login_manager
+from scheduler.models.models import User, Courses, Assignment, Exam
+from scheduler.models.forms import LoginForm, RegisterForm, NewCourseForm, NewExamForm, NewAssignmentForm
+from datetime import date
+from datetime import time
+from datetime import datetime, timedelta
+    
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# VIEWS
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+        return '<h1>Invalid username or password combination</h1>'
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data,
+                        university=form.university.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    user_courses = Courses.query.filter_by(user_id=current_user.get_id()).order_by(Courses.course_code.desc()).all()
+    user_exams = Exam.query.filter_by(user_id=current_user.get_id()).order_by(Exam.date.asc()).all()
+    user_asgs = Assignment.query.filter_by(user_id=current_user.get_id()).order_by(Assignment.due_date.asc()).all()
+    #filter_after = datetime.now() - timedelta(days=5)
+    today = datetime.today()
+    alert_date = today + timedelta(days=5)
+    #asg_alerts = Assignment.query.filter(Assignment.due_date <= filter_after).all()
+    asg_alerts = Assignment.query.filter(Assignment.due_date <= alert_date, Assignment.user_id==current_user.get_id(), Assignment.complete==False).all()
+    exam_alerts = Exam.query.filter(Exam.date <= alert_date, Exam.date >= today-timedelta(days=1), Exam.user_id==current_user.get_id()).all()
+    num_tasks = len(asg_alerts) + len(exam_alerts)
+
+    return render_template('dashboard.html', name=current_user.username, courses=user_courses, exams=user_exams,
+                           assignments=user_asgs,asg_alerts=asg_alerts, exam_alerts=exam_alerts,num_tasks=num_tasks)
+
+
+@app.route('/dashboard/courses')
+@login_required
+def courses():
+    user_courses = Courses.query.filter_by(user_id=current_user.get_id()).order_by(Courses.course_code.desc()).all()
+    # Dominic 18/11- user course data query
+    return render_template('courses.html', name=current_user.username, courses=user_courses,add_state=False,edit_state=False, form=None)
+
+
+@app.route('/dashboard/courses/add', methods=['GET', 'POST'])
+@login_required
+def addCourse():
+    form = NewCourseForm()
+    if form.validate_on_submit():
+        course = Courses(course_code=form.code.data, course_name=form.title.data,
+                         lecturer=form.lecturer.data, location=form.location.data, user_id=current_user.get_id())
+        db.session.add(course)
+        db.session.commit()
+        return redirect(url_for('courses'))
+    return render_template('courses.html', form=form,add_state=True)
+
+
+@app.route('/dashboard/courses/<c_id>', methods=['GET', 'POST'])
+@login_required
+def deleteCourse(c_id):
+    course = Courses.query.filter_by(c_id=c_id).first()
+    db.session.delete(course)
+    db.session.commit()
+    return redirect(url_for('courses'))
+
+
+@app.route('/dashboard/exams')
+@login_required
+def exams():
+    user_exams = Exam.query.filter_by(user_id=current_user.get_id()).order_by(Exam.date.asc()).all()
+    # Dominic 18/11- user exam data query
+    return render_template('exams.html', exams=user_exams, form=None, edit_state=False,add_state=False)
+
+
+@app.route('/dashboard/updateexam/<e_id>', methods=['GET','POST'])
+@login_required
+def updateExam(e_id):
+    edit_state=True
+    e = Exam.query.get(e_id)
+    form = NewExamForm(course_code=e.course_code, exam_name=e.exam_name, weighting=e.weighting, date=e.date, time=e.time, duration=e.duration,location=e.location)
+
+    if form.validate_on_submit():
+        Exam.query.filter_by(e_id=e_id).update(dict(course_code=form.course_code.data, exam_name=form.exam_name.data, weighting=form.weighting.data,date=form.date.data,duration=form.duration.data,location=form.location.data))
+        db.session.commit()
+        return redirect(url_for('exams'))
+    return render_template("exams.html", edit_state=True, form=form)
+
+
+@app.route('/dashboard/exams/<e_id>', methods=['GET', 'POST'])
+@login_required
+def deleteExam(e_id):
+    exam = Exam.query.filter_by(e_id=e_id).first()
+    db.session.delete(exam)
+    db.session.commit()
+    return redirect(url_for('exams')) 
+ 
+@app.route('/dashboard/exams/add', methods=['GET', 'POST'])
+@login_required
+def addExam():
+    form = NewExamForm()
+    if form.validate_on_submit():  # Needs too check if course exists first
+        exam = Exam(course_code=form.course_code.data,exam_name=form.exam_name.data, weighting=form.weighting.data,
+                    date=form.date.data, time=form.time.data, duration=form.duration.data, location=form.location.data,
+                    user_id=current_user.get_id())
+        db.session.add(exam)
+        db.session.commit()
+        return redirect(url_for('exams'))
+    return render_template('exams.html', add_state=True, edit_state=False,form=form)
+
+
+@app.route('/dashboard/assignments')
+@login_required
+def assignments():
+    user_asgs = Assignment.query.filter_by(user_id=current_user.get_id()).order_by(Assignment.due_date.asc()).all()
+    # Dominic 18/11- user assignment data query
+    return render_template('assignments.html', assignments=user_asgs, edit_state=False, add_state=False, form=None)
+
+
+@app.route('/dashboard/assignments/add', methods=['GET', 'POST'])
+@login_required
+def addAssignments():
+    form = NewAssignmentForm()
+    if form.validate_on_submit():  # Needs too check if course exists first
+        assignment = Assignment(course_code=form.course_code.data, asg_name=form.asg_name.data,
+                                weighting=form.weighting.data, due_date=form.due_date.data,
+                                due_time=form.due_time.data, user_id=current_user.get_id())
+        db.session.add(assignment)
+        db.session.commit()
+        return redirect(url_for('assignments'))
+    return render_template('assignments.html', edit_state=False, add_state=True, form=form)
+
+
+@app.route('/dashboard/updateassignment/<a_id>', methods=['GET','POST'])
+@login_required
+def updateAssignment(a_id):
+    a = Assignment.query.get(a_id)
+    form = NewAssignmentForm(course_code=a.course_code,asg_name=a.asg_name, weighting=a.weighting, due_date=a.due_date, due_time=a.due_time)
+
+    if form.validate_on_submit():
+        Assignment.query.filter_by(a_id=a_id).update(dict(course_code=form.course_code.data, asg_name=form.asg_name.data,weighting=form.weighting.data, due_date=form.due_date.data,due_time=form.due_time.data))
+        db.session.commit()
+        return redirect(url_for('assignments'))
+    return render_template("assignments.html", add_state=False, edit_state=True, form=form)
+
+
+@app.route('/dashboard/markcomplete/<a_id>', methods=['GET', 'POST'])
+@login_required
+def markCompleteAssignment(a_id):
+    Assignment.query.filter_by(a_id=a_id).update(dict(complete=True))
+    db.session.commit()
+    return redirect(url_for('assignments'))
+
+
+@app.route('/dashboard/assignments/<a_id>', methods=['GET', 'POST'])
+@login_required
+def deleteAssignment(a_id):
+    assignment = Assignment.query.filter_by(a_id=a_id).first()
+    db.session.delete(assignment)
+    db.session.commit()
+    return redirect(url_for('assignments'))
+
+
+@app.route('/dashboard/completed')
+@login_required
+def completed():
+    user_asgs = Assignment.query.filter_by(user_id=current_user.get_id()).order_by(Assignment.due_date.asc()).all()
+    # Dominic 18/11- user assignment data query
+    return render_template('completed.html', assignments=user_asgs, edit_state=False, add_state=False, form=None)
+
+
+@app.route('/dashboard/completed/<a_id>', methods=['GET', 'POST'])
+@login_required
+def deleteCompleted(a_id):
+    assignment = Assignment.query.filter_by(a_id=a_id).first()
+    db.session.delete(assignment)
+    db.session.commit()
+    return redirect(url_for('completed'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':  # Dominic - is this necessary?
+    app.run(debug=True)
+
